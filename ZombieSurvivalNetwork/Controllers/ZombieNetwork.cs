d750579ef7c1036547ebfefb5f0f40ee883a97aa
Ethan Simson <ethan.simson@yousmartthing.com>
2022-01-06 19:02:41 +0000

    Start of MongoDB
    added survivor model

---
+++ ZombieSurvivalNetwork/Controllers/ZombieNetwork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZombieSurvivalNetwork.Controllers
+{
+    public class ZombieNetwork
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ ZombieSurvivalNetwork/Models/Survivor.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace ZombieSurvivalNetwork.Models
+{
+    public class Survivor
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+    }
+}
